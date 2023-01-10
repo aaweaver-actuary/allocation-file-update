b4f21f167e8953f305e031bbd843858951440d23
aaweaver-actuary <53792857+aaweaver-actuary@users.noreply.github.com>
2023-01-10 20:31:09 +0000

    put the repo together, not much else

---
+++ src/lookups.py
@@ -0,0 +1,7 @@
+
+# data frame columns:
+df_columns = ['ay','lob','cum_rpt_loss','cum_paid_dcce','ep','sel_loss_ratio','sel_dcce_ratio']
+
+# file stem:
+resv_applications = r'O:\STAFFHQ\SYMDATA\Actuarial\Reserving Applications\IBNR Allocation'
+
